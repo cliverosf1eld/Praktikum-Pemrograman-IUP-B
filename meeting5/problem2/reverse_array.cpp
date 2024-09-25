1621c068c8b60dd0365a4b310486b6ae2318cebc
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-09-25 21:08:00 +0700

    Update reverse_array.cpp

--- meeting5/problem2/reverse_array.cpp
+++ meeting5/problem2/reverse_array.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm>  
 
-std::vector<int> reverseVector(const std::vector<int>& input) 
-    {
+std::vector<int> reverseVector(const std::vector<int>& input) {
     std::vector<int> reversed(input);
     std::reverse(reversed.begin(), reversed.end());
     return reversed;
+}
+
+int main() {
+    int n;
+    
+    std::cout << "Enter the number of elements in the array: ";
+    std::cin >> n;
+
+    std::vector<int> vec(n);
+    
+    std::cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> vec[i]; 
     }
-int main ()
-{
-    std::vector<int> vec = {2, 4, 6, 8, 10};
+
     std::vector<int> reversedVec = reverseVector(vec);
-     std::cout << "Reversed Vector: ";
-    for (int i : reversedVec) 
-    {
+
+    std::cout << "Reversed Vector: ";
+    for (int i : reversedVec) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
+
     return 0;
 }
