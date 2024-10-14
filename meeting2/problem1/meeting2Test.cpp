60e464dbb488ff404f9ca3c17e5ccd66101017f9
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-10-14 07:29:11 +0000

    finish test case

---
+++ meeting2/problem1/meeting2Test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int gross, tax, installment, insurance, netsalary;
+    string fname, mname, lname;
+
+    cout << "Enter your first Name, middle Name & last Name" << endl;
+    cin >> fname >> mname >> lname;
+    cout << "Gross Salary: ";
+    cin >> gross;
+    cout << "Installment: ";
+    cin >> installment;
+    cout << "Insurance: ";
+    cin >> insurance;
+    tax = 0.2 * gross;
+    netsalary = gross - tax - installment - insurance;
+    cout << "Payslip for Employee" << endl;
+    cout << "--------------------" << endl;
+    cout << "Name: " << fname << " " << mname << " " << lname << " " << endl;
+    cout << "Gross Salary = " << gross << endl;
+    cout << "Tax (20%) = " << tax << endl;
+    cout << "Installment = " << installment << endl;
+    cout << "Insurance = " << insurance << endl;
+    cout << "Net Salary = " << netsalary << endl;
+
+    cout << "Test successfully run";
+    return 0;
+}
