83ad450317f8571ee93ffd077ff47ac02242db56
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-10-14 20:33:16 +0700

    Update meeting6Test.cpp

--- meeting6/meeting6Test.cpp
+++ meeting6/meeting6Test.cpp
@@ -15,20 +15,17 @@ int main(){
     int i;
     struct data student[50];
     
-    cout << "Number of students (max is 50): ";
-    cin >> n;
+    n = 3;
 
     for (i=0; i<n; i++){
-        cout << "Enter the student's NIM: ";
-        cin >> student[i].nim;
-        cout << "Enter the student's UTS result: ";
-        cin >> student[i].uts;
-        cout << "Enter the student's UTA result: ";
-        cin >> student[i].uta;
+        student[i].nim = 100 + i;
+        student[i].uts = 100;
+        student[i].uta = 50;
         student[i].avg = (student[i].uts + student[i].uta) / 2;
     }
     for (int i=0; i<n; i++){
         cout << "NIM: " << student[i].nim << endl << "Average : " << student[i].avg << endl;
     }
+    cout << "Test is successful" << endl;
     return 0;
 }
