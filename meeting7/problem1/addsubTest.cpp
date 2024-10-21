a7f6010687f56a3490c901a2238f9ad477c94373
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-10-21 19:54:20 +0700

    Update addsubTest.cpp

--- meeting7/problem1/addsubTest.cpp
+++ meeting7/problem1/addsubTest.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
 
+void addition (float a, float b){
+    cout << a << " + " << b << " = " << a + b;
+}
+void subtraction (float a, float b){
+    cout << a << " - " << b << " = " << a - b;
+}
+
+int main(){
+    float a, b;
+    char i, i1;
+    a = 5;
+    b = 10;
+    i = '+';
+    i1 = '-';
+    
+    if (i == '+'){
+        addition (a, b);
+    }
+    else if (i == '-'){
+        subtraction (a, b);
+    }
+    else
+    cout << "Invalid operation";
+    cout << endl;
+    
+    if (i1 == '+'){
+        addition (a, b);
+    }
+    else if (i1 == '-'){
+        subtraction (a, b);
+    }
+    else
+    cout << "Invalid operation";
+    
+    return 0;
+}
