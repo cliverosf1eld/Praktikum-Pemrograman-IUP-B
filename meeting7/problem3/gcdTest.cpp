f28706149045b2223902fb0b0de37f2092e0eb8a
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-10-21 20:23:03 +0700

    Update gcdTest.cpp

--- meeting7/problem3/gcdTest.cpp
+++ meeting7/problem3/gcdTest.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a, b;
+    a = 10;
+    b = 20;
+    
+    int result = gcd(a, b);
+    cout << "The GCD of " << a << " and " << b << " is " << result << endl;
+
+    return 0;
+}
