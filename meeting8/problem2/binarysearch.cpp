8f21fccbca54f1e49e7f71b9644b79eb26ed59af
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-10-30 09:48:06 +0700

    Add files via upload

---
+++ meeting8/problem2/binarysearch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool found = true;
+    long int data[] = {9960312699, 9963959682, 9950310962, 9970272750, 9970293945, 9952382180, 9965653989};
+    long int i=0, j = sizeof(data) / sizeof(data[0]), k, query = 9950310962;
+    while (!found&&1<=j){
+        k = (i+j)/2;
+        if (data[k]<query){
+            i = k + 1;
+        }
+        else if (data[k] == query) {
+            found = true;
+        }
+        else {
+            j = k - 1;
+        }
+    }
+    if (!found){
+        cout << "Not found";
+    }
+    else{
+        cout << "Found, value is 80";
+    }
+    return 0;
+}
