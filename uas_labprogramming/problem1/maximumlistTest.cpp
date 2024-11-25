5536398c9482a6825604f00d08924d9ae568cc29
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-11-25 13:53:15 +0700

    Update maximumlistTest.cpp

--- uas_labprogramming/problem1/maximumlistTest.cpp
+++ uas_labprogramming/problem1/maximumlistTest.cpp
@@ -1,1 +1,11 @@
+#include <iostream> //question 4: finding maximum in a list
+#include <algorithm>
+using namespace std;
 
+int main(){
+    int al = 5;
+    int a[al] = {5, 3, 2, 1, 8};
+    sort(a, a + al);
+    
+    cout << "The maximum number on the list is " << a[al-1];
+}
