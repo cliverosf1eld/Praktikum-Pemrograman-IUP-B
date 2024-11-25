96478bf38216bcd2173e695b98597d5748d9d6e6
cliverosf1eld <gibranbasyir2006@gmail.com>
2024-11-25 08:29:39 +0700

    Update sumdigitsTest.cpp

--- uas practice/sumdigitsTest.cpp
+++ uas practice/sumdigitsTest.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+int main(){
+    int x, sum;
+    x = 252;
+    
+    while (x>0){
+        sum += x%10;
+        x /= 10;
+    }
+    cout << "Sum of the digits is " << sum;
+}
